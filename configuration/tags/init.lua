dff2a0658ad2fdb3820af437134eb579b75bed27
krazedkrish <krazedkrish@gmail.com>
2021-05-19 20:36:22 +1000

    change shadow and layouts

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -3,7 +3,18 @@ local tags = require('configuration.tags.tags')
 
 awful.layout.layouts = {
   awful.layout.suit.tile,
-  awful.layout.suit.max
+  -- -- awful.layout.suit.tile.left,
+  awful.layout.suit.tile.bottom,
+  -- -- awful.layout.suit.tile.top,
+  -- -- awful.layout.suit.fair,
+  -- awful.layout.suit.fair.horizontal,
+  -- -- awful.layout.suit.spiral,
+  awful.layout.suit.spiral.dwindle,
+  -- awful.layout.suit.max,
+  -- awful.layout.suit.spiral,
+  -- awful.layout.suit.max.fullscreen,
+  awful.layout.suit.magnifier,
+  awful.layout.suit.floating
 }
 
 awful.screen.connect_for_each_screen(
